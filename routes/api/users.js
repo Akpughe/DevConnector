c12b06a9fd7c3c17a7518080ee0c7d88c09dfb4c
David <davidakpughe2@gmail.com>
2020-03-27 18:38:36 +0100

    implementing JWT

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const gravatar = require('gravatar')
-const bcrypt = require('bcryptjs')
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator/check');
 const User = require('../../models/User');
 // @route POST api/user
@@ -33,29 +35,50 @@ router.post(
       let user = await User.findOne({ email });
 
       if (user) {
-       return res.status(400).json({ errors: [{ msg: 'User already exists' }] });
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'User already exists' }] });
       }
 
       // Get users gravatar
       const avatar = gravatar.url(email, {
-          s: '200',
-          r: 'pg',
-          d: 'mm'
-      })
-    
-      user = new User ({
-          name, email, avatar, password
-      })
+        s: '200',
+        r: 'pg',
+        d: 'mm'
+      });
+
+      user = new User({
+        name,
+        email,
+        avatar,
+        password
+      });
 
       //Encrypt password
 
       const salt = await bcrypt.genSalt(10);
 
-      user.password = await bcrypt.hash(password, salt)
+      user.password = await bcrypt.hash(password, salt);
 
-      await user.save()
+      await user.save();
 
       //Return JWT
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+
       res.send('User registered');
     } catch (err) {
       console.error(err.message);
