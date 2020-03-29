e5a79144cb4de1719835746357a3d94859614f7f
David <davidakpughe2@gmail.com>
2020-03-30 00:02:09 +0100

    add post routes

--- models/Post.js
+++ models/Post.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+// const Schema = mongoose.Schema;
 
 const PostSchema = new mongoose.Schema({
   user: {
-    type: Schema.Types.ObjectId,
-    ref: 'users'
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user'
   },
   text: {
     type: String,
@@ -19,16 +19,18 @@ const PostSchema = new mongoose.Schema({
   likes: [
     {
       user: {
-        types: Schema.Types.ObjectId,
-        ref: 'users'
+        type: mongoose.Schema.Types.ObjectId,
+
+        ref: 'user'
       }
     }
   ],
   comments: [
     {
       user: {
-        types: Schema.Types.ObjectId,
-        ref: 'users'
+        type: mongoose.Schema.Types.ObjectId,
+
+        ref: 'user'
       },
       text: {
         type: String,
