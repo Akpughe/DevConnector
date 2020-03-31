df72c15b299d2f04a40715cb2d4eb2db4570092d
David <davidakpughe2@gmail.com>
2020-03-31 22:45:59 +0100

    load user and set auth token

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/layout/Navbar';
@@ -7,25 +7,35 @@ import Alert from './components/layout/Alert';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 //Redux
-import {Provider } from 'react-redux'
-import store from './store'
+import { Provider } from 'react-redux';
+import store from './store';
+import { loadUser } from './actions/auth';
+import setAuthToken from './utils/setAuthToken'
 
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
 
-const App = () => (
-  <Provider store={store}>
-  <Router>
-    <>
-      <Navbar />
-      <Route exact path="/" component={Landing} />
-      <section className="container">
-        <Alert/>
-        <Switch>
-          <Route exact path='/register' component={Register} />
-          <Route exact path='/login' component={Login} />
-        </Switch>
-      </section>
-    </>
-  </Router>
-  </Provider>
-);
+const App = () => {
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, [])
+  return (
+    <Provider store={store}>
+      <Router>
+        <>
+          <Navbar />
+          <Route exact path="/" component={Landing} />
+          <section className="container">
+            <Alert />
+            <Switch>
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/login" component={Login} />
+            </Switch>
+          </section>
+        </>
+      </Router>
+    </Provider>
+  );
+};
 export default App;
